2c1706f83797d576f32876b3b06cc4caf99951f1
aasifshahzad <asif17186@outlook.com>
2024-03-08 12:37:48 +0500

    update

---
+++ uitclass/main.py
@@ -0,0 +1,41 @@
+#fastapi_neon/main.py
+
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+
+app = FastAPI(
+    title="Location Finder API", 
+    version="1.0.0",
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Development Server"
+        }
+        ])
+
+class Location(BaseModel):
+    name: str
+    location: str
+
+locations = {
+    "zia": Location(name="Zia", location="Karachi"),
+    "ali": Location(name="Ali", location="Lahore"),
+}
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# dependency function
+def get_location_or_404(name:str)->Location:
+    loc = locations.get(name.lower())
+    if not loc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No location found for {name}")
+    return loc
+
+
+@app.get("/location/{name}")
+def get_person_location(name:str, location: Annotated[Location, Depends(get_location_or_404)]):
+    return location
